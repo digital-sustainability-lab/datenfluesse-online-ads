449bb8a4e94b5cf6b6304eab37b6c5efa9a9d751
Dominic Schweizer <dominic.schweizer@inf.unibe.ch>
2022-06-03 08:30:12 +0200

    feat: prep script includes owner lineage

--- data/index.js
+++ data/index.js
@@ -7,6 +7,10 @@ const fs = require("fs");
 const parsedReport = papa.parse(
   fs.readFileSync("./per_site_network_report.csv", { encoding: "utf8" })
 );
+const thirdParyDomains = papa
+  .parse(fs.readFileSync("./3p_domains.csv", { encoding: "utf8" }))
+  .data.slice(1);
+
 const domainRows = parsedReport.data.slice(1);
 const domainMap = domainRows.reduce((map, row, index) => {
   if (!map[row[0]]) map[row[0]] = { thirdParties: [] };
@@ -20,6 +24,19 @@ console.log(domains);
 fs.writeFileSync("./domains.json", JSON.stringify(domains), {
   encoding: "utf8",
 });
+
 function createThirdPartyDomain(row) {
-  return { requestDomain: row[1], owner: row[2], ownerCountry: row[3] };
+  return {
+    requestDomain: row[1],
+    owner: row[2],
+    ownerCountry: row[3],
+    ownerLineage: getOwnerLineage(row[1]),
+  };
+}
+function getOwnerLineage(requestDomain) {
+  const thirdPartyInformation = thirdParyDomains.find(
+    (row) => row[1] === requestDomain
+  );
+  if (!thirdPartyInformation) return "unkown";
+  return thirdPartyInformation[4];
 }
