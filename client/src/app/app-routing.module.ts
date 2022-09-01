9503e0c8530da6aed358229bf05ae414087fb650
Nick <nick.soland@bfh.ch>
2022-09-01 16:54:43 +0200

    test: added component that tests if links in network can be filled with data

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HierarchBarComponent } from './hierarch-bar/hierarch-bar.component';
 import { NetworkComponent } from './network/network.component';
 import { BarchartDetails } from './barchart-details/barchart-details.component';
+import { TestChartComponent } from './test-chart/test-chart.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'bar', pathMatch: 'full' },
   { path: 'bar', component: HierarchBarComponent },
   { path: 'details', component: BarchartDetails },
   { path: 'network', component: NetworkComponent },
+  { path: 'test', component: TestChartComponent },
 ];
 
 @NgModule({
---
+++ client/src/app/test-chart/test-chart.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TestChartComponent } from './test-chart.component';
+
+describe('TestChartComponent', () => {
+  let component: TestChartComponent;
+  let fixture: ComponentFixture<TestChartComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TestChartComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
